35fb832cd533a4aa6b308309894d4f3600de6757
Calamity <NikitaYoutube2008@icloud.com>
2022-07-06 17:19:32 +0500

    updated plugin host & updated setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 echo Download purpur
-wget "https://api.purpurmc.org/v2/purpur/1.18.1/latest/download" -O purpur.jar -o /dev/null
+wget "https://api.purpurmc.org/v2/purpur/1.18.2/latest/download" -O purpur.jar -o /dev/null
 
 #Download plugins
 for i in $(cat plugins.txt)
